534efdd4e817349448587bd274fe86c8bea9678c
rafia9005 <rafia9005@gmail.com>
2024-02-13 11:39:49 +0700

    push vercel

--- main.js
+++ main.js
@@ -1,41 +1,39 @@
-import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
-import Base64 from 'base64-js';
-import MarkdownIt from 'markdown-it';
-import { maybeShowApiKeyBanner } from './gemini-api-banner';
-import './style.css';
+import {
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/generative-ai";
+import Base64 from "base64-js";
+import MarkdownIt from "markdown-it";
+import { maybeShowApiKeyBanner } from "./gemini-api-banner";
+import "./style.css";
 
-// 🔥 FILL THIS OUT FIRST! 🔥
-// 🔥 GET YOUR GEMINI API KEY AT 🔥
-// 🔥 https://makersuite.google.com/app/apikey 🔥
-let API_KEY = 'AIzaSyAX9UVhVVZ9zuJAKiH99mrw9tNe8CPqKH0';
+let API_KEY = "AIzaSyAX9UVhVVZ9zuJAKiH99mrw9tNe8CPqKH0";
 
-let form = document.querySelector('form');
+let form = document.querySelector("form");
 let promptInput = document.querySelector('input[name="prompt"]');
-let output = document.querySelector('.output');
+let output = document.querySelector(".output");
 
 form.onsubmit = async (ev) => {
   ev.preventDefault();
-  output.textContent = 'Generating...';
+  output.textContent = "Generating...";
 
   try {
     // Load the image as a base64 string
-    let imageUrl = form.elements.namedItem('chosen-image').value;
+    let imageUrl = form.elements.namedItem("chosen-image").value;
     let imageBase64 = await fetch(imageUrl)
-      .then(r => r.arrayBuffer())
-      .then(a => Base64.fromByteArray(new Uint8Array(a)));
+      .then((r) => r.arrayBuffer())
+      .then((a) => Base64.fromByteArray(new Uint8Array(a)));
 
-    // Assemble the prompt by combining the text with the chosen image
     let contents = [
       {
-        role: 'user',
+        role: "user",
         parts: [
-          { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
-          { text: promptInput.value }
-        ]
-      }
+          { inline_data: { mime_type: "image/jpeg", data: imageBase64 } },
+          { text: promptInput.value },
+        ],
+      },
     ];
-
-    // Call the gemini-pro-vision model, and get a stream of results
     const genAI = new GoogleGenerativeAI(API_KEY);
     const model = genAI.getGenerativeModel({
       model: "gemini-pro-vision",
@@ -48,18 +46,17 @@ form.onsubmit = async (ev) => {
     });
 
     const result = await model.generateContentStream({ contents });
-
-    // Read from the stream and interpret the output as markdown
     let buffer = [];
     let md = new MarkdownIt();
     for await (let response of result.stream) {
       buffer.push(response.text());
-      output.innerHTML = md.render(buffer.join(''));
+      output.innerHTML = md.render(buffer.join(""));
     }
   } catch (e) {
-    output.innerHTML += '<hr>' + e;
+    output.innerHTML += "<hr>" + e;
   }
 };
 
-// You can delete this once you've filled out an API key
+
+
 maybeShowApiKeyBanner(API_KEY);
